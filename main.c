57a0d2f7b00a63b88a4fc3f1bbbbd57519305475
AzizTheGazele552 <therobot.mehrez@gmail.com>
2024-10-21 13:37:46 -0400

    test                                                                                                                     MISSING FEATURES: SUN, SAND DUNES, DOUBLE BUFFER TO NOT RE-PROJECT SAME PIXELS AS LAST FRAME + DRAW AT POSITION IMPLEMENTATION ALGORITHM BY CHATGPT USING ESCAPE CODES TO POSITION CURSOR + MULTIPLE FRAMES OF THESE

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
-#include <math.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <conio.h>
+#include <math.h>
+#include <windows.h>
 
 #define F_BLACK "\x1b[30m"
 #define F_RED "\x1b[31m"
@@ -40,122 +42,397 @@
 #define B_BRIGHTCYAN "\x1b[106m"
 #define B_BRIGHTWHITE "\x1b[107m"
 
+char buffer[40000] = {0};
+double el_initializeCanvas, el_bFill, el_bCircleEdge, el_bSun, el_fBloomTriangle, el_fOutlineTriangle, el_fForwardFaceTriangle, el_showImage, el_colorizePixel,
+        el_total,
+        el_timeBetweenFrames = 0;
+LARGE_INTEGER start, end, freq, startTotal, endTotal;
+
+void startTimer() {
+    QueryPerformanceFrequency(&freq); // Get the frequency of the high-resolution timer
+    QueryPerformanceCounter(&start); // Get the current counter value (start time)
+}
+
+void stopTimer() {
+    QueryPerformanceCounter(&end); // Get the current counter value (end time)
+}
+
+double getElapsedTimeInMicroseconds() {
+    return (double) (end.QuadPart - start.QuadPart) * 1000000.0 / freq.QuadPart;
+}
+
+void startTimerTotal() {
+    QueryPerformanceFrequency(&freq); // Get the frequency of the high-resolution timer
+    QueryPerformanceCounter(&startTotal); // Get the current counter value (start time)
+}
+
+void stopTimerTotal() {
+    QueryPerformanceCounter(&endTotal); // Get the current counter value (end time)
+}
+
+double getElapsedTimeInMicrosecondsTotal() {
+    return (double) (endTotal.QuadPart - startTotal.QuadPart) * 1000000.0 / freq.QuadPart;
+}
+
 struct pixel {
     char symbol;
     unsigned char bRGB;
-    //using last 4 bits, which indicate, in order, brightness, redness, greeness, blueness
 };
 
-void colorizePixel(char stringBuffer[], unsigned char bRGB, int i) {
-    for (int k = 0; k < 5; k++) {
-        char var[5] = "\x1b[96m";
-        stringBuffer[i * 6 - 6 + k] = var[k];
+void adjustFrameRate(float targetFrameRate) {
+    stopTimerTotal();
+    el_timeBetweenFrames = getElapsedTimeInMicrosecondsTotal();
+    usleep(1000000.0f / targetFrameRate - el_timeBetweenFrames);
+    startTimerTotal();
+}
+
+void colorizePixel(char stringBuffer[], unsigned char bRGB, int j, int i) {
+    startTimer();
+
+    for (int k = 1; k <= 5; k++) {
+        stringBuffer[(j * 936) + (i * 6 - 6 + k - 1)] = "\x1b[96m"[k - 1];
     }
 
     unsigned char brightness = bRGB >> 3;
-    stringBuffer[i * 6 - 3] = brightness ? 9 : 3;
+    stringBuffer[(j * 936) + (i * 6 - 4)] = brightness ? '9' : '3';
 
     unsigned char rgb = (bRGB & 0b00000111);
     switch (rgb) {
-        case 0: stringBuffer[i * 6 - 2] = 0; break;
-        case 2: stringBuffer[i * 6 - 2] = 2; break;
-        case 5: stringBuffer[i * 6 - 2] = 5; break;
-        case 1: stringBuffer[i * 6 - 2] = 4; break;
-        case 3: stringBuffer[i * 6 - 2] = 6; break;
-        case 4: stringBuffer[i * 6 - 2] = 1; break;
-        case 6: stringBuffer[i * 6 - 2] = 3; break;
-        default: printf("DEBUG: (bRGB & 0b00000111) = %d\n", rgb); break;
+        case 0: stringBuffer[(j * 936) + (i * 6 - 3)] = '0';
+            break;
+        case 1: stringBuffer[(j * 936) + (i * 6 - 3)] = '4';
+            break;
+        case 2: stringBuffer[(j * 936) + (i * 6 - 3)] = '2';
+            break;
+        case 3: stringBuffer[(j * 936) + (i * 6 - 3)] = '6';
+            break;
+        case 4: stringBuffer[(j * 936) + (i * 6 - 3)] = '1';
+            break;
+        case 5: stringBuffer[(j * 936) + (i * 6 - 3)] = '5';
+            break;
+        case 6: stringBuffer[(j * 936) + (i * 6 - 3)] = '3';
+            break;
+        case 7: stringBuffer[(j * 936) + (i * 6 - 3)] = '7';
+            break;
+        default: printf("DEBUG: (bRGB & 0b00000111) = %d\n", rgb);
+            fflush(stdout);
+            break;
     }
+
+    stopTimer();
+    el_colorizePixel = getElapsedTimeInMicroseconds();
 }
 
+void showImage(struct pixel canvas[41][156]) {
+    startTimer();
 
-void showImage(struct pixel canvas[40][156]) {
-
-    for (int j = 0; j < 40; j++) {
-        char stringBuffer[936] = {0};
-        for     (int i = 1; i <= 156; i++) {
-            stringBuffer[i * 6 - 1] = canvas[j][i - 1].symbol;
-            colorizePixel(stringBuffer, canvas[j][i - 1].bRGB, i);
+    char stringBuffer[38376] = {0};
+    for (int j = 0; j < 41; j++) {
+        for (int i = 1; i <= 156; i++) {
+            stringBuffer[(j * 936) + (i * 6 - 1)] = canvas[j][i - 1].symbol;
+            colorizePixel(stringBuffer, canvas[j][i - 1].bRGB, j, i);
         }
-        fwrite(stringBuffer, 936, 1, stdout);
     }
+    system("cls");
+    //write(1, "\x1b[2J\x1b[H", 7); // 1 being stdout
+    fwrite(stringBuffer, 38376, 1,stdout);
+    //write(1, stringBuffer, 38376); // 1 being stdout
+
+    stopTimer();
+    el_showImage = getElapsedTimeInMicroseconds();
 }
 
+typedef struct {
+    int x;
+    int y;
+} Point;
 
-void circleEdge(struct pixel canvas[40][156]) {
-    for (int j = 0; j < 40; j++) {
+float triangleArea(Point p1, Point p2, Point p3) {
+    return 0.5 * abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
+}
+
+void fForwardFaceTriangle(struct pixel canvas[41][156], int iheight) {
+    startTimer();
+
+    // Vertices
+    Point A = {78, 41 - iheight};
+    Point B = {78 - (iheight * 2), 41};
+    Point C = {78 + (iheight * 2), 41};
+
+    // Calculate total area of triangle specified by vertices
+    float areaOrig = triangleArea(A, B, C);
+    for (int j = 0; j < 41; j++) {
         for (int i = 0; i < 156; i++) {
-            int iFromOrigin = i - 78;
-            int jFromOrigin = j * (5 / 2) - 20 * (5 / 2);
-            int distFromOriginSquared = iFromOrigin * iFromOrigin + jFromOrigin * jFromOrigin;
+            if (j > (A.y - 1 - (10 / iheight)) && i > B.x && i < C.x) {
+                Point P = {i, j}; // Current pixel's coordinate
 
-            if (distFromOriginSquared < 2035 && distFromOriginSquared > 1950) {
-                canvas[j][i].symbol = '^';
-                canvas[j][i].bRGB = 0b00001110;
+                float area1 = triangleArea(P, B, C);
+                float area2 = triangleArea(A, P, C);
+                float area3 = triangleArea(A, B, P);
+
+                // Is point inside triangle?
+                if (area1 + area2 + area3 <= areaOrig) {
+                    if (j > (41 - iheight + iheight / 10)) {
+                        canvas[j][i].symbol = '='; // = - _ WHICH?
+                        canvas[j][i].bRGB = 0b0111;
+                    } else {
+                        canvas[j][i].symbol = 'U';
+                        canvas[j][i].bRGB = 0b1110;
+                    }
+                }
             }
         }
     }
+
+
+    stopTimer();
+    el_fForwardFaceTriangle = getElapsedTimeInMicroseconds();
 }
 
-void backgroundFill(struct pixel canvas[40][156]) {
+void fOutlineTriangle(struct pixel canvas[41][156], int iheight) {
+    startTimer();
+
+    // Vertices
+    Point A = {78, 40 - iheight};
+    Point B = {78 - (iheight * 2) - 1, 40};
+    Point C = {78 + (iheight * 2) + 1, 40};
+
+    // Calculate total area of triangle specified by vertices
+    float areaOrig = triangleArea(A, B, C);
+    for (int j = 0; j < 41; j++) {
+        for (int i = 0; i < 156; i++) {
+            if (j > (A.y - 1) && i > B.x && i < C.x) {
+                Point P = {i, j}; // Current pixel's coordinate
+
+                float area1 = triangleArea(P, B, C);
+                float area2 = triangleArea(A, P, C);
+                float area3 = triangleArea(A, B, P);
+
+                // Is point inside triangle?
+                if (area1 + area2 + area3 <= areaOrig) {
+                    canvas[j][i].symbol = '*';
+                    canvas[j][i].bRGB = 0b1110;
+                }
+            }
+        }
+    }
+
+
+    stopTimer();
+    el_fOutlineTriangle = getElapsedTimeInMicroseconds();
+}
+
+void fBloomTriangle(struct pixel canvas[41][156], int iheight) {
+    startTimer();
+
+    // Vertices
+    Point A = {77, 39 - iheight};
+    Point B = {77 - (iheight * 2) - 3, 41};
+    Point C = {77 + (iheight * 2) + 3, 41};
+
+    // Calculate total area of triangle specified by vertices
+    float areaOrig = triangleArea(A, B, C);
+    for (int j = 0; j < 41; j++) {
+        for (int i = 0; i < 156; i++) {
+            if (j > (A.y - 1) && i > B.x && i < C.x) {
+                Point P = {i, j}; // Current pixel's coordinate
+
+                float area1, area2, area3 = 0;
+                if (i < 78) {
+                    area1 = triangleArea(P, B, C);
+                    area2 = triangleArea(A, P, C);
+                    area3 = triangleArea(A, B, P);
+                } else {
+                    A.x = 78;
+                    B.x = 78 - (iheight * 2) - 3;
+                    C.x = 78 + (iheight * 2) + 3;
+
+                    area1 = triangleArea(P, B, C);
+                    area2 = triangleArea(A, P, C);
+                    area3 = triangleArea(A, B, P);
+                }
+
+                // Is point inside triangle?
+                if (area1 + area2 + area3 <= areaOrig) {
+                    if (j == 39 - iheight && i > 77) {
+                        canvas[j][i].symbol = '|';
+                        canvas[j][i].bRGB = 0b1110;
+                    } else if (j < (43 - iheight) && j != (39 - iheight))
+                        if (i < 78) {
+                            canvas[j][i].symbol = '\\';
+                            canvas[j][i].bRGB = 0b1110;
+                        } else {
+                            canvas[j][i].symbol = '/';
+                            canvas[j][i].bRGB = 0b1110;
+                        }
+                    else if (i < 78 && j != (39 - iheight)) {
+                        canvas[j][i].symbol = '>';
+                        canvas[j][i].bRGB = 0b1110;
+                    } else if (j != (39 - iheight)) {
+                        canvas[j][i].symbol = '<';
+                        canvas[j][i].bRGB = 0b1110;
+                    }
+                }
+            }
+        }
+    }
+
+
+    stopTimer();
+    el_fBloomTriangle = getElapsedTimeInMicroseconds();
+}
+
+void bSun(struct pixel canvas[41][156], int iheight) {
+    startTimer();
+
+    for (int j = 0; j < 41; j++) {
+        for (int i = 0; i < 156; i++) {
+            int iFromOrigin = i - 78;
+            int jFromOrigin = j * (5 / 2) - (41 - iheight) * (5 / 2);
+            int distFromOriginSquared = iFromOrigin * iFromOrigin + jFromOrigin * jFromOrigin;
+
+            if (distFromOriginSquared < iheight * 20) {
+                canvas[j][i].symbol = 'O';
+                canvas[j][i].bRGB = 0b1111;
+            }
+        }
+    }
+
+    stopTimer();
+    el_bSun = getElapsedTimeInMicroseconds();
+}
+
+void bCircleEdge(struct pixel canvas[41][156]) {
+    startTimer();
+
+    for (int j = 0; j < 41; j++) {
+        for (int i = 0; i < 156; i++) {
+            if (i < 78) {
+                if ((canvas[j][i + 1].symbol == '#' && canvas[j][i].symbol != '#') ||
+                    (canvas[j + 1][i].symbol == '#' && canvas[j - 1][i].symbol != '(' && canvas[j - 1][i].symbol != '#' && canvas[j][i - 1].symbol != '#' &&
+                     canvas[j][i].symbol != '#')) {
+                    canvas[j][i].symbol = '(';
+                    canvas[j][i].bRGB = 0b1110;
+                }
+            } else if ((canvas[j][i].symbol != '#' && canvas[j][i - 1].symbol == '#') ||
+                       (canvas[j + 1][i].symbol == '#' && canvas[j - 1][i].symbol != ')' && canvas[j - 1][i].symbol != '#' && canvas[j][i + 1].symbol != '#' &&
+                        canvas[j][i].symbol != '#')) {
+                canvas[j][i].symbol = ')';
+                canvas[j][i].bRGB = 0b1110;
+            }
+        }
+    }
+
+    stopTimer();
+    el_bCircleEdge = getElapsedTimeInMicroseconds();
+}
+
+void bFill(struct pixel canvas[41][156], int iheight) {
     //MULTIPLY X BY RATIO TO GET ACCURATE LENGTH 5:2
-    //ORIGIN IS (78,20)
-    for (int j = 0; j < 40; j++) {
+    //ORIGIN IS (78,20.5)
+
+    startTimer();
+
+    for (int j = 0; j < 41; j++) {
         for (int i = 0; i < 156; i++) {
             int iFromOrigin = i - 78;
-            int jFromOrigin = j * (5 / 2) - 20 * (5 / 2);
+            int jFromOrigin = j * (5 / 2) - (41 - iheight) * (5 / 2);
             int distFromOriginSquared = iFromOrigin * iFromOrigin + jFromOrigin * jFromOrigin;
 
-            if (distFromOriginSquared > 2000) {
-                if (i % 2 == 0)
-                    canvas[j][i].symbol = '^';
+            if (distFromOriginSquared > iheight * 100) {
+                if ((i + (j % 2) * 2) % 4 == 0)
+                    canvas[j][i].symbol = '+';
                 else
-                    canvas[j][i].symbol = '.';
+                    canvas[j][i].symbol = ' ';
 
-                canvas[j][i].bRGB = 0b00001110;
+                canvas[j][i].bRGB = 0b1000;
             }
         }
     }
+
+    stopTimer();
+    el_bFill = getElapsedTimeInMicroseconds();
 }
 
-void initializeCanvas(struct pixel canvas[40][156]) {
+void initializeCanvas(struct pixel canvas[41][156]) {
+    startTimer();
+
     for (int i = 0; i < 156; i++) {
-        for (int j = 0; j < 40; j++) {
-            canvas[j][i].symbol = '+';
-            canvas[j][i].bRGB = 0b00001110;
+        for (int j = 0; j < 41; j++) {
+            canvas[j][i].symbol = '#';
+            canvas[j][i].bRGB = 0b1100;
         }
     }
+
+    stopTimer();
+    el_initializeCanvas = getElapsedTimeInMicroseconds();
 }
 
 void drawOutput(int iheight) {
-    struct pixel canvas[40][156]; //156x40 = pixel position
+    struct pixel canvas[41][156] = {}; //156x41 = pixel position
+    unsigned char frameCounter = 0; //Aiming for 16 total frames at 4 fps, for a total of 4 seconds of run time
+    float targetFrameRate = 3.0f;
 
-    system("cls");
     initializeCanvas(canvas);
-    backgroundFill(canvas);
-    circleEdge(canvas);
-    showImage(canvas);
+    startTimerTotal();
+    while (1) {
+        if (kbhit())
+            if (getch() == ' ')
+                break;
+        fflush(stdin);
+
+        switch (frameCounter) /*NOLINT(*-multiway-paths-covered)*/ {
+            case 0:
+                bFill(canvas, iheight);
+                bCircleEdge(canvas);
+                bSun(canvas, iheight);
+                fBloomTriangle(canvas, iheight);
+                fOutlineTriangle(canvas, iheight);
+                fForwardFaceTriangle(canvas, iheight);
+                adjustFrameRate(targetFrameRate);
+                showImage(canvas);
+                break;
+            default: printf("FRAME COUNTER ERROR\nDEBUG: frameCounter = %i\n", frameCounter);
+                fflush(stdout);
+                break;
+        }
+    }
 }
 
 int main(void) {
-    float height = -1;
+    setvbuf(stdout, buffer,_IOLBF, sizeof(buffer));
+    // Default buffer for stdout is too small to fully contain stringBuffer later, so we increase it
+
+    float height = 0;
     int iheight = 0;
 
+
     while (1) {
-        printf("Veuillez saisir la hauteur de la pyramide : \n");
+        fwrite("Veuillez saisir la hauteur de la pyramide : \n", 45, 1,stdout);
+        fflush(stdout);
         scanf("%f", &height);
 
         iheight = (int) height;
-        if (height > 1 && height == iheight) {
+        if (height > 5 && height < 40 && height == iheight) {
             break;
         }
 
-        printf("\n\t<Veuillez saisir un nombre valide et reessayer.>\n\n");
+        fwrite("\n\t<Veuillez saisir un nombre valide et reessayer.>\n\n", 52, 1,stdout);
+        fflush(stdout);
         fflush(stdin);
-        height = -1;
     }
 
+    fflush(stdin);
     drawOutput(iheight);
+    system("cls");
+
+    el_total = (el_bFill + el_bCircleEdge + el_fBloomTriangle + el_showImage) / 1000.0f;
+    printf("\x1b[97mDEBUG: ELAPSED TIME IN EACH FUNCTION :\n\t\t\t"
+           "initializeCanvas = %.3lf ms\n\t\t\tbfill = %.3lf ms\n\t\t\tbCircleEdge = %.3lf ms\n\t\t\tbSun = %.3lf\n\t\t\tfBloomTriangle = %.3lf ms\n\t\t\t"
+           "fOutlineTriangle = %.3lf ms\n\t\t\tfForwardFaceTriangle = %.3lf ms\n\t\t\tshowImage = %.3lf ms\n\t\t\tcolorizePixel = %.3lf ms\n\t\t\ttotal = %.3lf ms\n",
+           el_initializeCanvas / 1000, el_bFill / 1000, el_bCircleEdge / 1000, el_bSun / 1000, el_fBloomTriangle / 1000, el_fOutlineTriangle / 1000,
+           el_fForwardFaceTriangle / 1000, el_showImage / 1000, el_colorizePixel / 1000, el_total);
+    fflush(stdout);
     system("pause");
     return 0;
 }
